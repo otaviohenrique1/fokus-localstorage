12076a7e7da79cd9130b977d9a889aaea6119699
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-11-06 18:07:54 -0300

    commit 5

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,8 @@ const formLabel = document.querySelector(".app__form-label");
 const textArea = document.querySelector(".app__form-textarea");
 const cancelFormTaskBtn = document.querySelector(".app__form-footer__button--cancel");
 
+localStorage.setItem("quantidade", 10);
+
 // Lista de tarefas
 let tarefas = [
   // {
@@ -82,3 +84,92 @@ cancelFormTaskBtn.addEventListener("click", () => {
   formTask.classList.add('hidden');
   limparForm();
 });
+
+/*
+const taskListContainer = document.querySelector(".app__section-task-list");
+const formTask = document.querySelector(".app__form-add-task");
+const toggleFormTaskBtn = document.querySelector(".app__button--add-task");
+const formLabel = document.querySelector(".app__form-label");
+const textArea = document.querySelector(".app__form-textarea");
+const cancelFormTaskBtn = document.querySelector(".app__form-footer__button--cancel");
+
+// Lista de tarefas
+let tarefas = [
+  // {
+  //   descricao: 'Tarefa Concluída',
+  //   concluida: true
+  // },
+  // {
+  //   descricao: 'Tarefa Pendente',
+  //   concluida: false
+  // }
+];
+
+const taskIconSvg = `
+<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
+    fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="12" fill="#FFF" />
+    <path
+        d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+        fill="#01080E" />
+</svg>
+`;
+
+// Limpa o formulario
+const limparForm = () => {
+  textArea.value = "";
+  formTask.classList.add("hidden");
+};
+
+// Cria uma tarefa
+function createTask(tarefa) {
+  const li = document.createElement("li");
+  li.classList.add("app__section-task-list-item");
+
+  const svgIcon = document.createElement("svg");
+  svgIcon.innerHTML = taskIconSvg;
+
+  const paragraph = document.createElement("p");
+  paragraph.classList.add("app__section-task-list-item-description");
+  paragraph.textContent = tarefa.descricao;
+
+  li.appendChild(svgIcon);
+  li.appendChild(paragraph);
+
+  return li;
+}
+
+// Adiciona a lista na pagina as tarefas
+tarefas.forEach((task) => {
+  const taskItem = createTask(task);
+  taskListContainer.appendChild(taskItem);
+});
+
+// Mostra o formulario
+toggleFormTaskBtn.addEventListener("click", () => {
+  formLabel.textContent = "Adicionando tarefa";
+  formTask.classList.toggle("hidden");
+});
+
+// Acao do formulario
+formTask.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const task = {
+    descricao: textArea.value,
+    concluido: false
+  };
+  tarefas.push(task);
+  const taskItem = createTask(task);
+  taskListContainer.appendChild(taskItem)
+
+  limparForm();
+});
+
+// Esconde o formulario
+cancelFormTaskBtn.addEventListener("click", () => {
+  formTask.classList.add('hidden');
+  limparForm();
+});
+
+localStorage.setItem("quantidade", 10);
+*/
